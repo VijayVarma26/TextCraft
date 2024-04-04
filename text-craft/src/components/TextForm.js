9d828fadc807ad7686d628335b9b41dfd93c4e87
VijayVarma26 <varma26vijay@gmail.com>
2024-04-04 22:38:23 +0530

    Added Feature to Count in TextForm Component

--- text-craft/src/components/TextForm.js
+++ text-craft/src/components/TextForm.js
@@ -1,29 +1,43 @@
 import React, { useState } from "react";
 
 export default function TextForm() {
-    const [text, setText] = useState('');
+  const [text, setText] = useState("");
 
-    const handleOnChange = (event) =>{
-        setText(event.target.value);
-    }
+  const handleOnChange = (event) => {
+    setText(event.target.value);
+  };
 
-    const toUpperCase = () =>{
-        setText(text.toUpperCase());
-    }
+  const toUpperCase = () => {
+    setText(text.toUpperCase());
+  };
 
-    const toLowerCase = () =>{
-        setText(text.toLowerCase());
-    }
-
+  const toLowerCase = () => {
+    setText(text.toLowerCase());
+  };
 
   return (
     <>
       <div class="form-group">
         <h2 for="comment">Enter Your Text Below:</h2>
-        <textarea class="form-control" value={text} onChange={handleOnChange} rows="8" id="textArea" placeholder="Enter You Text Here"></textarea>
-        <button className="btn btn-primary my-3" onClick={toUpperCase}>Convert to UpperCase</button>
-        <button className="btn btn-primary my-3 mx-2" onClick={toLowerCase}>Convert to UpperCase</button>
+        <textarea
+          class="form-control"
+          value={text}
+          onChange={handleOnChange}
+          rows="8"
+          id="textArea"
+          placeholder="Enter You Text Here"
+        ></textarea>
+        <button className="btn btn-primary my-3" onClick={toUpperCase}>
+          Convert to UpperCase
+        </button>
+        <button className="btn btn-primary my-3 mx-2" onClick={toLowerCase}>
+          Convert to UpperCase
+        </button>
+      </div>
+      <div className="container my-3 ">
+        <h3>Word Count = {text.split(" ").length}</h3>
+        <h3>Character Count = {text.length}</h3>
       </div>
     </>
   );
-} 
+}
