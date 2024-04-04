480b2c95e691e9e4a29387e9752779a91c65854e
VijayVarma26 <varma26vijay@gmail.com>
2024-04-04 20:31:52 +0530

    Create Text Form Component

--- text-craft/src/App.js
+++ text-craft/src/App.js
@@ -1,12 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
 import Navbar from './components/Navbar';
+import TextForm from './components/TextForm';
 
 function App() {
   return (
     <>
       <Navbar webTitle = 'TextCraft' />
-    
+      <div className="container my-3">
+        <TextForm />
+      </div>
     </>
   );
 }
--- text-craft/src/components/TextForm.js
+++ text-craft/src/components/TextForm.js
@@ -1,9 +1,27 @@
-import React from 'react'
+import React, { useState } from "react";
 
 export default function TextForm() {
+    const [text, setText] = useState();
+
+    const onChange = () =>{
+
+    }
+    const toUpperCase = () =>{
+
+    }
+
+    const toLowerCase = () =>{
+
+    }
+
+
   return (
-    <div>
-      
-    </div>
-  )
-}
+    <>
+      <div class="form-group">
+        <h2 for="comment">Enter Your Text Below:</h2>
+        <textarea class="form-control" rows="8" id="textArea" placeholder="Enter You Text Here"></textarea>
+        <button className="btn btn-primary my-3" >Convert to UpperCase</button>
+      </div>
+    </>
+  );
+} 
